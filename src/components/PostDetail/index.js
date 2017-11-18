5b2511a99825d1eb21626b39048e8dd528d6915e
dskline <dskline1@gmail.com>
2017-11-18 00:07:51 -0500

    fix: HTML should be rendered from markdown, because storing HTML is not full proof

--- src/components/PostDetail/index.js
+++ src/components/PostDetail/index.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { createElement } from 'react'
 import PropTypes from 'prop-types'
+import marksy from 'marksy'
 
 import { LazyImage, Dimensions } from 'src/elements/LazyImage'
 import Link from 'src/elements/Link'
+import Video from 'src/elements/Video'
 
 import './style.scss'
 
@@ -26,9 +28,6 @@ export default class PostDetail extends React.Component {
     if (post === undefined) {
       return <div>Post not found</div>
     }
-    const content = typeof post.content === 'string'
-      ? <div id='post-content' className='mt3 pt3 bt lh-copy' dangerouslySetInnerHTML={{__html: post.content}} />
-      : <div id='post-content' className='mt3 pt3 bt lh-copy'>{ post.content }</div>
     return (
       <div>
         {post.titleImageUrl &&
@@ -41,9 +40,25 @@ export default class PostDetail extends React.Component {
           <div className='pv2 i'>
             { new Date(post.createdAt).toLocaleDateString('en-us', dateFormat) }
           </div>
-          { content }
+          <div id='post-content' className='mt3 pt3 bt lh-copy'>
+            { this.convertMarkdownToHtml(post.content) }
+          </div>
         </div>
       </div>
     )
+  }
+  convertMarkdownToHtml (markdown) {
+    const compiled = marksy({
+      createElement,
+      elements: {
+        img (props) {
+          if (props.alt.indexOf('video') !== -1) {
+            return <Video src={props.src} />
+          }
+          return <LazyImage {...props} />
+        }
+      }
+    })(markdown)
+    return compiled.tree
   }
 }
--- src/pages/CreatePostPage/index.js
+++ src/pages/CreatePostPage/index.js
@@ -1,12 +1,10 @@
-import React, {createElement} from 'react'
+import React from 'react'
 import update from 'immutability-helper'
-import marksy from 'marksy'
 
 import SidebarTemplate from 'src/templates/SidebarTemplate'
 import CreatePostSidebar from './containers/CreatePostSidebar'
 import PostDetail from 'src/components/PostDetail'
 import { LazyImage } from 'src/elements/LazyImage'
-import Video from 'src/elements/Video'
 
 import './style.scss'
 import 'src/components/MenuBar/themes/white.scss'
@@ -15,7 +13,6 @@ export default class CreatePostPage extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      markdown: '',
       post: {
         title: 'New Post',
         createdAt: new Date().toString(),
@@ -31,7 +28,8 @@ export default class CreatePostPage extends React.Component {
         <div className='bg-image'>
           <LazyImage src={'blog-bgimage'} className='bg-monument' />
         </div>
-        <textarea placeholder={'## Put your markdown here'} value={this.state.markdown} onChange={this.postTextChanged} />
+        <textarea placeholder={'## Put your markdown here'} value={this.state.post.content} onChange={this.postTextChanged} />
+        <textarea value={JSON.stringify(this.state.post.content)} />
         <div className='bg-white pv4-l ph5-l br2 shadow-3'>
           { this.state.post.content &&
             <PostDetail post={this.state.post} />
@@ -42,25 +40,9 @@ export default class CreatePostPage extends React.Component {
   }
   postTextChanged (e) {
     this.setState(update(this.state, {
-      markdown: { $set: e.target.value },
       post: {
-        content: { $set: this.convertMarkdownToHtml(e.target.value) }
+        content: { $set: e.target.value }
       }
     }))
-  }
-
-  convertMarkdownToHtml (markdown) {
-    const compiled = marksy({
-      createElement,
-      elements: {
-        img (props) {
-          if (props.alt.indexOf('video') !== -1) {
-            return <Video src={props.src} />
-          }
-          return <LazyImage {...props} />
-        }
-      }
-    })(markdown)
-    return compiled.tree
   }
 }
