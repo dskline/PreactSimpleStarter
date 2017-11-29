6932cf7448b849693412bde34b255668f6b69a66
dskline <dskline1@gmail.com>
2017-11-29 17:29:20 -0500

    chore: slightly modify language

--- src/pages/HomePage/containers/ThisWebsiteContainer/index.js
+++ src/pages/HomePage/containers/ThisWebsiteContainer/index.js
@@ -33,19 +33,24 @@ export default class ThisWebsiteContainer extends React.Component {
         </div>
         <div className='flex flex-column flex-row-ns justify-around ph3 ph0-ns lh-copy'>
           <div>
-            <p>{'Welcome to my home page! This is my own personal playground where I experiment with new JavaScript ' +
-            'frameworks. Then I write blog posts about my experiences and helpful tips developing with them. My ' +
-            'other projects will be showcased here as well, so check back for those at a later time.'}</p>
+            <p>
+              { 'Welcome to my home page! This is my own personal playground where I experiment with JavaScript ' +
+              'frameworks and then blog about my experiences and helpful tips developing with them. My belief is ' +
+              'that staying up to date on the latest developer tools is important for building quality code and a ' +
+              'quality product.' }
+            </p>
             <ActionLink url='/blog'>
               The Blog
             </ActionLink>
           </div>
           <div className='w4 w5-l h2' />
           <div>
-            <p>{'This portfolio is a Progressive Web App, so it\'s able to function offline with Service Workers. ' +
-            'Google Lighthouse analyzes each page to find areas of improvement in performance over slow internet ' +
-            'connections. Blog posts are stored and fetched on a GraphQL backend, and all source code is publicly ' +
-            'listed on GitHub.'}</p>
+            <p>
+              { 'This portfolio is a Progressive Web App, so it\'s able to function offline with Service Workers. ' +
+              'Google Lighthouse analyzes each page to find areas of improvement in performance over slow internet ' +
+              'connections. Blog posts are stored and fetched on a GraphQL backend, and all source code is publicly ' +
+              'listed on GitHub.' }
+            </p>
             <ActionLink url='https://www.github.com/dskline/portfolio'>
               The Source Code
             </ActionLink>
